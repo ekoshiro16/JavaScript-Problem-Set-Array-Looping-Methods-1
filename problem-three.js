28447c52ed8ae7c06b7697afe8a1e5abee5c243d
ekoshiro16 <ekoshiro16@gmail.com>
2023-05-18 21:54:17 -0700

    fixed typos and added clarification to problem 3

--- problem-three.js
+++ problem-three.js
@@ -1,11 +1,23 @@
 /*
-Hey developers. Remember that nerd coder from the first problem? Well I'm her, but they call me the Anti-Elle. I have a master plan to infect all her code with not one, but TWO typos, and all with one single algorithm!! *evil laughter*
+Hey developers. Remember that nerd programmer from the first problem? Well I'm her baby sister, and I've got a great plan. I just need from you an algorithm that can infect all her code with not one, but TWO typos, and all with one single algorithm!! *evil laughter*
 
 Using the appropriate array looping method, write some code that takes in an array of objects, where each object represents a chunk of code. A sample array is provided below.
 
 Your code should add two key-value pairs to each object in the array. The first key-value pair will have a key called "typoOne" (feel free to give it any typo value you like), and the second key-value pair will have a key called "typoTwo" (with any value). 
 
 If you do everything right, the console.log at the bottom of this file should show two typo key-value pairs in each object in the array. 
+
+For example, if this were one of the objects in the ellesCode array:
+{
+    actualCode: "console.log('data...')"
+}
+
+Then, after your function runs, the above object could look like this: 
+{
+    actualCode: "console.log('data...')",
+    typoOne: "amiocmosicjaoid",
+    typoTwo: "asdjghkll"
+}
 */
 
 let ellesCode = [
